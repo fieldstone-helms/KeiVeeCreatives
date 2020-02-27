3e25eddb04056ab319f5da2183858d8b8b7b2f45
Ess Wango <warmbow@gmail.com>
2020-02-27 15:07:09 +0300

    Initial commit after successful test deplyment

---
+++ lib/contstants.dart
@@ -0,0 +1,23 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class Constants {
+
+}
+
+Color primaryColor(){
+  return Color.fromARGB(255, 0, 121, 120);
+}
+
+Color textColor(){
+  return Colors.black54;
+}
+
+double h3FontSize(){
+  return 18;
+}
+
+double h1FontSize(){
+  return 42;
+}
